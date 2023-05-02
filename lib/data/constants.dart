f1453fb1b1f922476c3957fe6462166e9854e530
vahid <vahidmohammadisan@gmail.com>
2023-05-02 18:45:48 +0330

    chenaged icon

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,6 +1,6 @@
 class Urls {
   static const String baseUrl =
-      "https://opensheet.elk.sh/1qo8CmFIrsiicVac-PUyIgLOuyx-eUzqozemjlV03IsY/sheet1";
+      "https://script.googleusercontent.com/macros/echo?user_content_key=TKM8hJCqcVb5WomZkRFySBDfhdaPlTn6HqQ8zyz4WNIx9NicQ3xHsQPad2y6rz2R4vpS61Qd0jNkJzYQBdAv9AwASBzm-GYSm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFh-xv9t49nKgZZyFj9W6BbI8QcTYsnFphdI51y1Odz_mNlTQo6fvxZm2JtTVwMN0PB7zvR3CUcBGQMZvMo-EUypPWp7YHZz8w&lib=MIY145OvP2s7WQnFjGTNUkTbsQ0CRhLAj";
 
   static const String imageUrl = "https://docs.google.com/uc?id=";
 }
